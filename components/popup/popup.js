b539741a6330a4fb0c1482a6f2a1f7080a0ddf88
litianzhao <tianzhao.li@bitmain.com>
2016-01-12 22:51:16 +0800

    popup 页面添加了价格闪动效果

--- components/popup/popup.js
+++ components/popup/popup.js
@@ -1,73 +1,104 @@
 require('./popup.less');
-
 require('../../lib/bootstrap');
 
 const _ = require('lodash');
+const Vue = require('vue');
+const constants = require('../../lib/constants');
+const config = require('../../config');
+const utils = require('../../lib/utils');
+const storage = require('../../lib/storage');
 
-$(function() {
-    chrome.storage.local.get('rates', ({ rates }) => {
-        Vue.filter('CNY2USD', v => {
-            return v * rates.USD;
-        });
+Vue.filter('format', (v, size, withSign) => {
+    if (withSign == null) withSign = false;
+    var num = v.toFixed(size);
+    if (withSign && v >= 0) num = '+' + num;
+    return num;
+});
 
-        Vue.filter('USD2CNY', v => {
-            return v / rates.USD;
-        });
+const priceIndicator = {
+    props: ['v', 'rates', 'symbol', 'options'],
+    filters: {
 
-        Vue.filter('format', (v, size, withSign) => {
-            if (withSign == null) withSign = false;
-            var num = v.toFixed(size);
-            if (withSign && v >= 0) num = '+' + num;
-            return num;
-        });
+    },
+    data() {
+        return {
+            change: 0
+        }
+    },
+    template: `
+<div class="price-indicator" v-el:container>
+    <i class="glyphicon glyphicon-arrow-up" v-show="change > 0"></i>
+    <i class="glyphicon glyphicon-arrow-right" v-show="change == 0"></i>
+    <i class="glyphicon glyphicon-arrow-down" v-show="change < 0"></i>
+    {{ options.price.preferCurrency == 'USD' ? '$' : '¥' }} {{ convert(v / 1e3, symbol.currency_type, options.price.preferCurrency) | format 2 }}
+    /
+    {{ options.price.preferCurrency == 'USD' ? '¥' : '$' }} {{ convert(v / 1e3, symbol.currency_type, options.price.preferCurrency == 'USD' ? 'CNY' : 'USD') | format 2 }}
+</div>
+`,
+    methods: {
+        convert(value, fromSymbol, toSymbol){
+            return this.rates[toSymbol] / this.rates[fromSymbol] * value;
+        }
+    },
+    ready(){
+        const container = this.$els.container;
 
-        new Vue({
-            el: '.markets',
-            data: {
-                markets: []
-            },
-            ready() {
-                let symbols;
+        container.addEventListener('animationend', function animate() {
+            container.classList.remove('active-green');
+            container.classList.remove('active-red');
+        }, false);
+    },
+    watch: {
+        v(nv, ov) {
+            console.log('nv = %d, ov = %d', nv, ov);
+            if (ov == null) {
+                this.change = 0;
+            } else {
+                this.change = nv - ov;
+            }
 
-                chrome.storage.local.get(['markets', 'symbols'], items => {
-                    symbols = items.symbols.filter(e => e.coin_type == 'BTC');
-                    this.markets = symbols.map(e => {
-                        let o = _.extend(items.markets.data[e.symbol], e, {
-                            change: 0
-                        });
-                        if (o.currency_type == 'USD') {
-                            o.open = Vue.filter('USD2CNY')(o.open);
-                            o.last = Vue.filter('USD2CNY')(o.last);
-                            o.high = Vue.filter('USD2CNY')(o.high);
-                            o.low = Vue.filter('USD2CNY')(o.low);
-                        }
-                        return o;
-                    });
-                });
+            // 添加 active 类
+            let color;
+            if (this.change > 0 && this.options.price.changeColor == constants.RED_UP_GREEN_DOWN) color = 'red';
+            else if (this.change > 0 && this.options.price.changeColor == constants.RED_DOWN_GREEN_UP) color = 'green';
+            else if (this.change < 0 && this.options.price.changeColor == constants.RED_UP_GREEN_DOWN) color = 'green';
+            else if (this.change < 0 && this.options.price.changeColor == constants.RED_DOWN_GREEN_UP) color = 'red';
 
-                chrome.storage.onChanged.addListener(changes => {
-                    if (changes.markets) {
-                        this.markets = symbols.map(e => {
-                            let change = changes.markets.newValue.data[e.symbol].last - changes.markets.oldValue.data[e.symbol].last;
-                            let o = _.extend(changes.markets.newValue.data[e.symbol], e, {
-                                change: 0
-                            });
-                            if (o.currency_type == 'USD') {
-                                o.open = Vue.filter('USD2CNY')(o.open);
-                                o.last = Vue.filter('USD2CNY')(o.last);
-                                o.high = Vue.filter('USD2CNY')(o.high);
-                                o.low = Vue.filter('USD2CNY')(o.low);
-                            }
+            color && this.$els.container.classList.add('active-' + color);
+        }
+    }
+};
 
-                            Vue.nextTick(() => {
-                                o.change = change;
-                            });
+new Vue({
+    el: '.markets',
+    data: {
+        markets: {},
+        symbols: [],
+        rates: {},
+        options: {}
+    },
+    filter: {
 
-                            return o;
-                        });
-                    }
-                });
+    },
+    components: {
+        priceIndicator
+    },
+    methods: {
+    },
+    ready() {
+        chrome.storage.onChanged.addListener(changes => {
+            if (changes.markets) {
+                console.log('new markets', changes.markets.newValue.data);
+                _.extend(this.markets, changes.markets.newValue.data);
             }
         });
-    });
+
+        return Promise.join(storage.getMarkets(), storage.getSymbolAndRates(), storage.getOptions())
+            .then(([ markets, { symbols, rates }, options ]) => {
+                this.markets = markets.data;
+                this.symbols = symbols;
+                this.rates = rates;
+                this.options = options;
+            });
+    }
 });
