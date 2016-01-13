b52cc6acb59c6a5037516592aec55c54b02794ed
litianzhao <tianzhao.li@bitmain.com>
2016-01-13 18:15:43 +0800

    添加了 lookup

---
+++ components/lookup/look.js
@@ -0,0 +1,233 @@
+
+var worth,type,oDiv,targetElement,element;
+var islink=false;
+document.body.addEventListener("click",isSelect, false);
+document.body.addEventListener("mouseover",isLink, false);
+
+/*用途：判断是否移入A标签*/
+function isLink(){
+    if (window.event.srcElement.tagName == "A") {
+        var preworth = worth;
+        worth = $.trim(window.event.srcElement.innerText);
+        if (looktype()) {
+            clearSlct();//清除选中元素
+            targetElement = window.event.target;
+            islink = true;
+            panel();
+            $(targetElement).on('mouseleave',function(){
+                if(islink){
+                    var x1=$(oDiv).offset().left;//ODiv X1
+                    var y1=$(targetElement).offset().top;//targetElement y1
+                    var mx=event.clientX+document.body.scrollLeft;//鼠标X
+                    var my = event.clientY + document.body.scrollTop;//鼠标Y
+                    if (my >= y1 && mx >= x1 && mx < (mx + 330)) {
+                        return;
+                    }
+                    else {
+                        $("#oDiv").hide(700);
+                    }
+                }
+            })
+
+        } else {
+            worth = preworth;
+            return false;
+        }
+
+    }
+}
+
+/*用途：判断是否选中*/
+function isSelect(){
+    if(window.getSelection){
+        var preworth=worth;
+        worth = String(window.getSelection());
+        worth = worth.replace(/^\s*/, "").replace(/\s*$/, "");
+        if(worth=="") return;
+        if(looktype()){
+            targetElement=window.event.target;
+            islink=false;
+            panel();
+        }else{
+            worth = preworth;
+            return false;
+        }
+    }
+}
+/*用途：创建一个面板*/
+function panel(){
+    var pagewidth=document.body.clientWidth-event.clientX;
+    var clientX = event.clientX+document.body.scrollLeft;
+    //var clientY = event.clientY+document.body.scrollTop;
+    var clientY =$(targetElement).offset().top + $(targetElement).height();
+    if(oDiv){oDiv.remove()}
+    oDiv=document.createElement('div');
+    oDiv.id="oDiv";// 赋值ID
+    document.body.appendChild(oDiv);
+    document.body.addEventListener("click",lookover,false);
+    document.getElementById('oDiv').addEventListener('click', stopEvent, true);
+    document.getElementById('oDiv').addEventListener('mouseleave', function(){
+        if(islink) {
+            $("#oDiv").hide(700);
+        }
+    }, false);
+    var html='<div id="oDiv_top" class="oDiv_top"></div>'+
+             '<div class="oDiv_point"></div>'+
+             '<div id="P_title">'+
+                '<div id="typelink"><a class="typeinfo" href="https://chain.btc.com/'+type+'/'+worth+'" target="_blank"><span class="typename">'+dicbook(type)+'</span><span class="sanjiao"></span></a></div>'+
+                '<div id="close"></div>'+
+             '</div>'+
+             '<div class="topline"></div>'+
+             '<div id="content_data">'+
+                '<div id="loading"><div style="float:left;">loading</div><div class="load_point"></div></div>'+
+                '<table id="datainfo" class="datainfo"></table>'+
+             '</div>' +
+             '<div class="oDivline"></div>'+
+             '<a href="https://chain.btc.com" target="_blank" class="btc_logo"></a>';
+    $("#oDiv").append(html);
+   // $("#oDiv_top").click(function(){targetElement.click();})
+    for (var item in element) {
+        var itemname=item.split("btc_")[1];
+        var  tr='<tr>' +
+                     '<td class="td1" style="height:20px!important;font-size:13px!important;">' + dicbook(itemname) + '</td>' +
+                     '<td class="td2" style="height:20px!important;font-size:13px!important;"><div id='+item+'></div></td>' +
+                '</tr>'
+        $("#datainfo").append(tr);
+    }
+    if(pagewidth<270){
+        oDiv.style.right=10+'px';// 指定创建的DIV在文档中距离左侧的位置
+        //$('.oDiv_point').css("margin-left",310-pagewidth+"px");
+    }
+    else {
+        oDiv.style.left=(clientX-7)+'px';// 指定创建的DIV在文档中距离左侧的位置
+    }
+    oDiv.style.top=(clientY+9)+'px';// 指定创建的DIV在文档中距离顶部的位置
+    getdata();
+    //Drag("oDiv"); //拖动窗口
+}
+
+/*用途：http获取数据*/
+function getdata(){
+    var url='';
+    if(lng=="zn"){
+        url='https://btcapp.api.btc.com/v1/search/'+worth+'?user_lang=zh-cn';
+    } else{
+        url='https://btcapp.api.btc.com/v1/search/'+worth+'?user_lang=en';
+    }
+    $.ajax({
+        type: "get",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        url: url,
+        beforeSend:function(xhr){
+             $("#loading").show();
+        },
+        success: function (data) {
+            var result = data.data;
+            if (data.error_no == "0") {
+                if (result.type == "block") {//高度
+                    var Orphaned="";
+                    if(result.data.chain_id!=0){
+                        Orphaned=dicbook("Orphaned");
+                        if(lng=="zn"){//孤块
+                            $("#datainfo").addClass("Orphaned");
+                        } else{
+                            $("#datainfo").addClass("Orphaned_en");
+                        }
+                    }
+                    $("#btc_height").html("<a target='_blank' href='https://chain.btc.com/block/"+result.data.height+"' >"+result.data.height+""+Orphaned+"</a>");
+                    $("#btc_confirm").text(result.data.confirm);
+                    $("#btc_time").text(localTime(result.data.time));
+                    $("#btc_size").text(formatnum(result.data.size)+" Bytes");
+                    $("#btc_txcount").text(formatnum(result.data.n_tx));
+                    if(result.data.relayed_by=="Unknown"){$("#btc_Relayed").html(result.data.relayed_by)}
+                    else{$("#btc_Relayed").html("<a target='_blank' href='https://chain.btc.com/stats/pool/"+result.data.relayed_by+"'>"+result.data.relayed_by+"</a>");}
+                }
+                if (result.type == "address") {//地址
+                    $("#btc_Balance").text((parseFloat(result.data.final_balance)/floatnum).toFixed(8).toString());
+                    $("#btc_total_received").text((parseFloat(result.data.total_received)/floatnum).toFixed(8).toString());
+                    if(result.data.unconfirmed_tx_count=="0"){$("#btc_txcount").text(result.data.n_tx)}
+                    else{ $("#btc_txcount").text(result.data.n_tx+"("+result.data.unconfirmed_tx_count+" "+dicbook("unconfirmed")+")")};
+                    $("#btc_past_month_tx_count").text(result.data.past_month_tx_count);
+                    $("#btc_first_tx_timestamp").html("<a target='_blank' href='https://chain.btc.com/tx/"+result.data.first_tx_hash+"'>"+localTime(result.data.first_tx_timestamp)+"</a>");
+                    if(result.data.n_tx=="0"){
+                        $("#btc_past_month_tx_count").parent().parent().remove();
+                        $("#btc_first_tx_timestamp").parent().parent().remove();
+                    }
+                    $("#btc_Impress").parent().prev().html("");
+                    if(result.data.impressions_count=="0"){
+                        $("#btc_Impress").parent().parent().remove();
+                    }
+                    else{
+                        if(lng=="zn"){//印象
+                            $("#btc_Impress").html("<a target='_blank' href='https://chain.btc.com/address/"+result.data.address+"'>印象"+result.data.impressions_count+"条</a>");
+                        } else{
+                            $("#btc_Impress").html("<a target='_blank' href='https://chain.btc.com/address/"+result.data.address+"'>"+result.data.impressions_count+" Impressions</a>");
+                        }
+                    }
+                }
+                if (result.type == "tx") {//交易
+                    $("#btc_timestamp").text(localTime(result.data.timestamp));
+                    $("#btc_confirm").text(result.data.confirm);
+                    $("#btc_fee").text((parseFloat(result.data.fee)/floatnum).toFixed(8).toString() );
+                    $("#btc_height").html("<a target='_blank' href='https://chain.btc.com/block/"+result.data.height+"'>"+result.data.height+"</a>");
+                    if(result.data.is_coinbase){ //coinbase 交易
+                        $("#btc_fee").text(((result.data.block_rewards+parseFloat(result.data.fee))/floatnum).toFixed(8).toString());
+                        $("#btc_fee").parent().prev().html(dicbook("reward"));
+                        $("#relayed_tr").remove();
+                        var  tr='<tr id="relayed_tr">' +
+                            '<td class="td1" style="height:20px!important;font-size:13px!important;">' + dicbook("Relayed") + '</td>';
+                             if(result.data.relayed_by=="Unknown"){
+                               tr+='<td class="td2" style="height:20px!important;font-size:13px!important;"><div id="Relayed">'+result.data.relayed_by+'</div></td></tr>';
+                             }
+                             else{
+                               tr+='<td class="td2" style="height:20px!important;font-size:13px!important;"><div id="Relayed"><a target="_blank" href="https://chain.btc.com/stats/pool/'+result.data.relayed_by+'">'+result.data.relayed_by+'</a></div></td></tr>';
+                             }
+                        $("#datainfo").append(tr);
+                        if(lng=="zn"){$("#datainfo").addClass("coinbase");}
+                        else{$("#datainfo").addClass("coinbase_en");}
+                    }
+                    if(result.data.confirm<1) { //未确认交易
+                        $("#btc_timestamp").text("-");
+                        if(lng=="zn"){$("#datainfo").addClass("unconfirm");}
+                        else{$("#datainfo").addClass("unconfirm_en");}
+                        $("#btc_confirm").text("0");
+                        $("#btc_height").text("-");
+                    }
+                }
+            }
+            else {
+                $("#typelink").html("");
+                $(".P_title").addClass("errortype");
+                $(".topline").remove();
+                $(".oDivline").remove();$(".btc_logo").remove();
+                $("#content_data").html("<div class='error_info'><div>"+dicbook('ErrorOne')+"</div><div>"+dicbook('ErrorTwo')+"</div><a href='https://chain.btc.com' target='_blank' class='btc_logo'></a></div>");
+            }
+        },
+        timeout: 20000,
+        complete:function(xhr){
+            $("#loading").remove();
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            $("#typelink").html("");
+            $(".P_title").addClass("errortype");
+            $(".topline").remove();
+            $(".oDivline").remove();$(".btc_logo").remove();
+            $("#content_data").html("<div class='error_info'><div>"+dicbook('ErrorOne')+"</div><div>"+dicbook('ErrorTwo')+"</div><a href='https://chain.btc.com' target='_blank' class='btc_logo'></a></div>");
+        }
+    });
+
+}
+/*用途：停止引发不该引发的click事件。*/
+var stopEvent = function(event){
+    e = event || window.event;
+    if(window.event.target.id=="close"){return;}
+    else if(window.event.target.id=="oDiv_top"){return;}
+    else if(e.stopPropagation){
+        e.stopPropagation();
+    }else {
+        e.cancelBubble = true;
+    }
+};
+
+
---
+++ components/lookup/tool.js
@@ -0,0 +1,155 @@
+/**
+ ** Created by wen on 15/12/4.
+ */
+
+/*用途：自动返回中英文字符*/
+var lng;
+var floatnum=parseFloat(Math.pow(10,8));
+function dicbook(field){
+    var zn={
+        "block":"块详情",
+        "address": "地址详情",
+        "tx":"交易详情",
+        "height": "高度",
+        "confirm": " 确认数",
+        "size": "大小",
+        "txcount": "交易数量",
+        "time": "时间",
+        "Relayed": "播报方",
+        "Balance": "余额",
+        "total_received": "总接收",
+        "fee":"矿工费",
+        "timestamp": "时间",
+        "past_month_tx_count":"最近30天交易数量",
+        "first_tx_timestamp":"初次交易时间",
+        "unconfirmed":"未确认",
+        "Orphaned block":"(孤块)",
+        "ErrorOne":"未找到符合查询的数据",
+        "ErrorTwo":"更多数据请访问",
+        "Impression":"印象",
+        "strip":"条",
+        "Impress":"",
+        "reward":"块奖励",
+    }
+    var en={
+        "block":"Block Info",
+        "address":"Address Info",
+        "tx":"Transaction Info",
+        "height":"Height",
+        "confirm": "Confirm",
+        "size": "Size",
+        "txcount": "Tx Count",
+        "time": "Time",
+        "Relayed": "Relaye By",
+        "Balance": "Balance",
+        "total_received": "Total Received",
+        "fee":"Fee",
+        "timestamp": "Time",
+        "past_month_tx_count":"Tx Count of Last 30 Days",
+        "first_tx_timestamp":"First Tx Time",
+        "unconfirmed":"Unconfirmed",
+        "Orphaned block":"(Orphaned Block)",
+        "ErrorOne":"Your search didn't match any records.",
+        "ErrorTwo":"If necessary, please visit <a href='https://chain.btc.com' target='_blank'>www.btc.com</a>",
+        "Impression":"Impression",
+        "strip":"条",
+        "Impress":"",
+        "reward":"Reward",
+    }
+    var lang = navigator.language || navigator.userLanguage;
+    if(lang.substr(0, 3) == "zh-") {
+        lng="zn"
+        return zn[field];
+    }else{
+        lng="en"
+        return en[field];
+    }
+}
+/*关闭面板*/
+function lookover(){
+    if(oDiv){
+        //if(window.event.target.contains(targetElement)){return;}
+        if(window.event.target===targetElement){return;}
+        if(window.event.target.id=="oDiv_top"){return;}
+        $("#oDiv").hide(700);
+    }
+}
+/*千分位数据格式化*/
+function formatnum(num) {
+    return (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,');
+}
+/*通过 document.selection.empty() 来清除选中的内容。*/
+var clearSlct= "getSelection" in window ? function(){
+    window.getSelection().removeAllRanges();
+} : function(){
+    document.selection.empty();
+};
+
+/*
+ 用途：时间戳怎么转成日期格式
+ */
+function localTime(nS) {
+    return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/,' ');
+}
+/*
+用途：最终匹配worth 的值是哪种类型(address,block,Tx)
+如果通过验证返回该值类型,否则返回false
+*/
+function looktype(){
+   if(isBlock()){
+       type="block";
+   } else {
+       if (isTx()) {
+           type = "tx";
+       } else {
+           if (isAddress()) {
+               type = "address";
+           } else {
+               return;
+           }
+       }
+   }
+        if(type=="block"){element = {"btc_height": "", "btc_confirm": "", "btc_time": "", "btc_size": "", "btc_txcount": "", "btc_Relayed": ""};}
+        if(type=="address"){element = {"btc_Balance": "", "btc_total_received": "", "btc_txcount": "","btc_past_month_tx_count": "","btc_first_tx_timestamp": "","btc_Impress":""};}
+        if(type=="tx"){element = {"btc_timestamp":"","btc_confirm":"","btc_fee":"","btc_height":""};}
+
+   return true;
+}
+/*
+用途：检查输入字符串是否一个address类型
+如果通过验证返回true,否则返回false
+*/
+function isAddress(){
+    //var fg=require('bitcore').Address.isValid(worth);
+    if (require('bitcore').Address.isValid(worth)){
+        return true;
+    }else {
+        return false;
+    }
+}
+/*
+ 用途：检查输入字符串是否一个block类型
+ 如果通过验证返回true,否则返回false
+ */
+function isBlock(){
+    var regu = "^[0]{8}[0-9a-f]{56}$";
+    var re = new RegExp(regu);
+    if (re.test(worth)){
+        return true;
+    }else {
+        return false;
+    }
+}
+/*
+ 用途：检查输入字符串是否一个block类型
+ 如果通过验证返回true,否则返回false
+ */
+function isTx(){
+    var regu = "^[0-9a-fA-F]{64}$";
+    var re = new RegExp(regu);
+    if (re.test(worth)){
+        return true;
+    }else {
+        return false;
+    }
+}
